9e6cbe659362b4be2c4c9e2f41f3565b91842b43
mwyoung <mnyou@DESKTOP-G8BFJVV>
2015-10-29 22:02:06 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mnyou/stopwatch/StopwatchActivity.java
@@ -0,0 +1,49 @@
+package com.example.mnyou.stopwatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class StopwatchActivity extends AppCompatActivity {
+    private int seconds = 0;
+    private boolean running = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+    }
+
+    public void onClickStart(View v) {
+        running = true;
+    }
+
+    public void onClickStop(View v) {
+        running = false;
+    }
+
+    public void onClickReset(View v)
+    {
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimer()
+    {
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
+        int hours = seconds /3600;
+        int minutes = (seconds%3600) / 60;
+        int secs = seconds % 60;
+
+        String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+
+        timeView.setText(time);
+
+        if (running)
+        {
+            seconds++;
+        }
+
+    }
+}
